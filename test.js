ef50917d58a7c739165c82f40f44dd7129fd3201
souparnaPersonal <souparna555@gmail.com>
2024-11-09 16:04:13 +0530

    first

---
+++ test.js
@@ -0,0 +1,5 @@
+var person = {
+    name: 'sta',
+    age: 20
+};
+console.log(person.age);
---
+++ test.ts
@@ -0,0 +1,12 @@
+type Person = {
+    name:string,
+    age:number
+
+}
+
+const person : Person = {
+    name:'sta',
+    age:20
+}
+
+console.log(person.age)
